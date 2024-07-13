a0576c3c8895a0cbad4bb2951cf5dffcc1d0bd00
Aditya-Matta <princeaditya.1922@gmail.com>
2024-07-13 19:29:01 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,28 @@
+//  Primitive 
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id == anotherId);
+
+const bigNumber = 322391320097638162530010283618625182n
+
+
+
+// Reference(Non-primitive)
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "aditya",
+    age: 19,
+}
+
