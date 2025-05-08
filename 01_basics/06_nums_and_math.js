ac6bee960bd6d976b71f2839875c75c4240a050c
Aditya-Matta <princeaditya.1922@gmail.com>
2025-05-08 14:00:41 +0000

    num rev

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -23,6 +23,8 @@ const hundreds = 1000000
 // console.log(Math.ceil(4.4));
 // console.log(Math.min(4, 3, 6, 8));
 
+// console.log(4**3);
+
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
