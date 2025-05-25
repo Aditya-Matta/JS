a261910a96db3a0416f14cb89a8b92c108d4029a
Aditya-Matta <princeaditya.1922@gmail.com>
2025-05-25 10:31:38 +0000

    constructor funcn

--- 10_class_and_oop/oop.js
+++ 10_class_and_oop/oop.js
@@ -8,12 +8,24 @@ const user = {
     }
 }
 
-console.log(user.username);
-// console.log(user.getUserDetails());
-// console.log(this)
+// console.log(user.username);
+// // console.log(user.getUserDetails());
+// // console.log(this)
 
+function User(username, loginCount, isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+    this.greeting = function(){
+        console.log(`Hello ${this.username}`)
+    }
 
+    return this
+}
 
-
+const userOne = new User("Aditya",14,true)
+const userTwo = new User("JS",19,true)
+console.log(userOne.constructor);
+// console.log(userTwo);
 
 
