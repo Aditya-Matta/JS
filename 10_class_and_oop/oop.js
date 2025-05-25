e8729e976a5f71af8357d26ffb130c9b8720ea8c
Aditya-Matta <princeaditya.1922@gmail.com>
2025-05-25 06:39:30 +0000

    obj oop

--- 10_class_and_oop/oop.js
+++ 10_class_and_oop/oop.js
@@ -1,7 +1,19 @@
 const user = {
     username : "Aditya",
     loginCount : 8,
-    signedIn : true
+    signedIn : true,
+    getUserDetails : function(){
+        // console.log("Got user details from database")
+        // console.log(`username: ${this.username}`)
+    }
 }
 
 console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this)
+
+
+
+
+
+
